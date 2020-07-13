0d557f4c3d04e409a05d4402176bfaa79b8473a6
lonelygoogle <lonelygoogle@qq.com>
2020-07-14 00:04:12 +0800

    babel

--- math.test.js
+++ math.test.js
@@ -12,25 +12,27 @@
 //     throw Error(`3-3 应该等于 ${expected}, 结果却是${result}`)
 // }
 
-function expect (result) {
-    return {
-        toBe: function (actual) {
-            if (result !== actual) {
-                throw new Error(`预期值和实际值不相等, 预期${actual}, 结果却是${result}`)
-                // throw Error(`3-3 应该等于 ${expected}, 结果却是${result}`)
-            }
-        }
-    }
-}
+// function expect (result) {
+//     return {
+//         toBe: function (actual) {
+//             if (result !== actual) {
+//                 throw new Error(`预期值和实际值不相等, 预期${actual}, 结果却是${result}`)
+//                 // throw Error(`3-3 应该等于 ${expected}, 结果却是${result}`)
+//             }
+//         }
+//     }
+// }
 
-function test (desc, fn) {
-    try {
-        fn()
-        console.log(`${desc}通过测试`)
-    } catch(e) {
-        console.log(`${desc}没有通过测试，${e}`)
-    }
-}
+// function test (desc, fn) {
+//     try {
+//         fn()
+//         console.log(`${desc}通过测试`)
+//     } catch(e) {
+//         console.log(`${desc}没有通过测试，${e}`)
+//     }
+// }
+
+import {add, minus, multi} from './math'
 
 test('测试加法 3+3', () => {
     expect(add(3,3)).toBe(6)
